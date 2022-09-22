274e205197db4bb911698e9f14b9afd7ce5eae57
kevinl424 <66340176+kevinl424@users.noreply.github.com>
2022-09-22 17:13:48 -0500

    Update Sound.cpp

--- Sound.cpp
+++ Sound.cpp
@@ -1,9 +1,3 @@
-// Sound.cpp
-// Runs on any computer
-// Sound assets based off the original Space Invaders 
-// Import these constants into your SpaceInvaders.c for sounds!
-// Jonathan Valvano
-// 1/17/2020
 #include <stdint.h>
 #include "Sound.h"
 #include "DAC.h"
