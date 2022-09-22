d0956f89e3e3b0d991fbf831306577dd85431b8d
kevinl424 <66340176+kevinl424@users.noreply.github.com>
2022-09-22 17:13:10 -0500

    Update SlidePot-1.cpp

--- SlidePot-1.cpp
+++ SlidePot-1.cpp
@@ -1,9 +1,3 @@
-// SlidePot.cpp
-// Runs on LM4F120/TM4C123
-// Provide functions that initialize ADC0
-// Student names: change this to your names or look very silly
-// Last modification date: change this to the last modification date or look very silly
-
 #include <stdint.h>
 #include "SlidePot.h"
 #include "../inc/tm4c123gh6pm.h"
@@ -13,7 +7,6 @@
 // Output: none
 // measures from PD2, analog channel 5
 void ADC_Init(void){ 
-//*** students write this ******
 	SYSCTL_RCGCADC_R |= 0x01; //start ADC clock
 	SYSCTL_RCGCGPIO_R |= 0x08; //activate Port D
 	while ((SYSCTL_RCGCGPIO_R & 0x08) == 0) {
@@ -46,7 +39,6 @@ void ADC_Init(void){
 // Output: 12-bit result of ADC conversion
 // measures from PD2, analog channel 5
 uint32_t ADC_In(void){  
-//*** students write this ******
 	uint32_t data;
 	ADC0_PSSI_R = 0x0008; //start ADC for seq 3
 	while ((ADC0_RIS_R & 0x08) == 0) {}
@@ -58,7 +50,6 @@ uint32_t ADC_In(void){
 // constructor, invoked on creation of class
 // m and b are linear calibration coeffients 
 SlidePot::SlidePot(uint32_t m, uint32_t b){
-//*** students write this ******
 // initialize all private variables
 // make slope equal to m and offset equal to b
 	slope = m;
@@ -66,7 +57,6 @@ SlidePot::SlidePot(uint32_t m, uint32_t b){
 }
 
 void SlidePot::Save(uint32_t n){
-//*** students write this ******
 // 1) save ADC sample into private variable
 // 2) calculate distance from ADC, save into private variable
 // 3) set semaphore flag = 1
@@ -75,7 +65,6 @@ void SlidePot::Save(uint32_t n){
 	flag = 1;
 }
 uint32_t SlidePot::Convert(uint32_t x){
-  //*** students write this ******
   // use calibration data to convert ADC sample to distance
   return (slope * x) / 4096 + offset; // replace this with solution
 }
@@ -88,13 +77,11 @@ void SlidePot::Sync(void){
 }
 
 uint32_t SlidePot::ADCsample(void){ // return ADC sample value (0 to 4095)
-  //*** students write this ******
   // return last calculated ADC sample
-  return ADC_In(); // replace this with solution
+  return ADC_In(); 
 }
 
 uint32_t SlidePot::Distance(void){  // return distance value (0 to 2000), 0.001cm
-  //*** students write this ******
   // return last calculated distance in 0.001cm
 	
   return distance; // replace this with solution
