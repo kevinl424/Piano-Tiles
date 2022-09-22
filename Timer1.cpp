eced54dcbd6d1f2dc8c21b3a748496b94042baec
kevinl424 <66340176+kevinl424@users.noreply.github.com>
2022-09-22 17:15:57 -0500

    Update Timer1.cpp

--- Timer1.cpp
+++ Timer1.cpp
@@ -1,26 +1,3 @@
-// Timer1.c
-// Runs on LM4F120/TM4C123
-// Use TIMER1 in 32-bit periodic mode to request interrupts at a periodic rate
-// Daniel Valvano
-// Last Modified: 1/17/2020 
-// You can use this timer only if you learn how it works
-
-/* This example accompanies the book
-   "Embedded Systems: Real Time Interfacing to Arm Cortex M Microcontrollers",
-   ISBN: 978-1463590154, Jonathan Valvano, copyright (c) 2013
-  Program 7.5, example 7.6
-
- Copyright 2013 by Jonathan W. Valvano, valvano@mail.utexas.edu
-    You may use, edit, run or distribute this file
-    as long as the above copyright notice remains
- THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
- OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
- VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
- OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- For more information about my classes, my research, and my books, see
- http://users.ece.utexas.edu/~valvano/
- */
 #include <stdint.h>
 #include "Timer1.h"
 #include "../inc/tm4c123gh6pm.h"
@@ -28,11 +5,7 @@
 void (*PeriodicTask1)(void);   // user function
 extern "C" void TIMER1A_Handler(void);
 
-// ***************** TIMER1_Init ****************
-// Activate TIMER1 interrupts to run user task periodically
-// Inputs:  task is a pointer to a user function
-//          period in units (1/clockfreq)
-// Outputs: none
+
 void Timer1_Init(void(*task)(void), uint32_t period){
   SYSCTL_RCGCTIMER_R |= 0x02;   // 0) activate TIMER1
   PeriodicTask1 = task;          // user function
